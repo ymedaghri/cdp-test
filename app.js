ce6ab989165c8b430c49c2a251ae3f71b0f58dd8
Youssef Medaghri-Alaoui <ymedaghri@gmail.com>
2023-11-16 07:24:47 +0100

    Parsing --filter=ry argument as an empty command

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const COUNT_COMMAND = {
-    parameter: "--count"
+    matcher: (arg) => {
+        const regex = /^--count$/;
+        const match = arg.match(regex);
+        return !!match
+    }
+}
+
+const FILTER_COMMAND = {
+    matcher: (arg) => {
+        const regex = /^--filter=(.+)$/;
+        const match = arg.match(regex);
+        return !!match
+    }
 }
 
 const parseCommandLineArgs = (args) => {
 
     const commands = args.map(arg => {
-        if (arg === COUNT_COMMAND.parameter) {
+        if (COUNT_COMMAND.matcher(arg)) {
             return COUNT_COMMAND
         }
-
+        if (FILTER_COMMAND.matcher(arg)) {
+            return FILTER_COMMAND
+        }
         return null
-    }).filter(command=>command!=null);
+    }).filter(command => command != null);
 
     return commands
 }
 
-module.exports = { parseCommandLineArgs, COUNT_COMMAND }
+module.exports = { parseCommandLineArgs, COUNT_COMMAND, FILTER_COMMAND }
--- app.test.js
+++ app.test.js
@@ -1,10 +1,10 @@
-const { parseCommandLineArgs, COUNT_COMMAND} = require("./app")
+const { parseCommandLineArgs, COUNT_COMMAND, FILTER_COMMAND } = require("./app")
 
 describe("Parsing command line arguments", () => {
 
-    test("Should parse a unary command line argument : count", () => {
+    test("Should parse a unary command line argument : --count", () => {
         //Given
-        const args = [COUNT_COMMAND.parameter]
+        const args = ["--count"]
 
         //When
         const commands = parseCommandLineArgs(args)
@@ -14,7 +14,7 @@ describe("Parsing command line arguments", () => {
         expect(commands[0]).toBe(COUNT_COMMAND)
     })
 
-    test("Should resturn an empty string when no known commands are passed as command line argument : uppercase", () => {
+    test("Should return an empty string when no known commands are passed as command line argument : uppercase", () => {
         //Given
         const args = ["--uppercase"]
 
@@ -24,4 +24,29 @@ describe("Parsing command line arguments", () => {
         //Then
         expect(commands).toHaveLength(0)
     })
+
+    test("Should parse a binary command line argument : --filter=", () => {
+        //Given
+        const args = ["--filter=ry"]
+
+        //When
+        const commands = parseCommandLineArgs(args)
+
+        //Then
+        expect(commands).toHaveLength(1)
+        expect(commands[0]).toBe(FILTER_COMMAND
+        )
+    })
+
+    test("Should not parse a binary command line argument who is presented as unary : --filter", () => {
+        //Given
+        const args = ["--filter"]
+
+        //When
+        const commands = parseCommandLineArgs(args)
+
+        //Then
+        expect(commands).toHaveLength(0)
+    }
+    )
 })
